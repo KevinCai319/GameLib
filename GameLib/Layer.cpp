d89271c4f18abaf6c8218bfeca920338eace0767
kevin16777216 <k1029384756c@gmail.com>
2021-08-30 21:59:36 -0400

    add stuff

--- GameLib/Layer.cpp
+++ GameLib/Layer.cpp
@@ -1,1 +1,34 @@
 #include "Layer.hpp"
+
+int Layer::update()
+{
+	return 0;
+}
+
+bool Layer::addEntity(sf::Drawable& layer)
+{
+	return false;
+}
+
+bool Layer::removeEntity(sf::Drawable& layer)
+{
+	return false;
+}
+
+bool Layer::modifyEntityTag(sf::Drawable& layer, std::string oldTag, std::string newTag)
+{
+	return false;
+}
+
+std::set<Layer>* Layer::getTag(std::string tag)
+{
+	return nullptr;
+}
+
+void Layer::recieve(int status)
+{
+}
+
+void Layer::notify(Layer& layer, int status)
+{
+}
