765112bc2455f89eb3a145a778138571fc904217
TahsinAhmed13 <tahsinahmed217@gmail.com>
2021-09-07 00:04:09 -0400

    byebye pointers

--- GameLib/WindowHandler.cpp
+++ GameLib/WindowHandler.cpp
@@ -5,20 +5,16 @@ WindowHandler::WindowHandler(int x, int y, const std::string& title) :
 	Handler(),
 	window(sf::VideoMode(x, y), title)
 {
-	// window = new sf::RenderWindow(sf::VideoMode(x,y), title);
 	tags.push_back("Window");
 }
 
 void WindowHandler::render()
 {
-	// window->clear();
 	window.clear(); 
 	if (running) 
 	{
-		// render(*window, sf::RenderStates::Default);
 		render(window, sf::RenderStates::Default);
 	}
-	// window->display();
 	window.display(); 
 }
 void WindowHandler::render(sf::RenderTarget& target, sf::RenderStates states) const
@@ -31,14 +27,11 @@ int WindowHandler::main()
 	int res = Handler::main();
 	if (res)return res;
 	sf::Event event;
-	// while (window->pollEvent(event))
 	while(window.pollEvent(event))
 	{
 		// Close window: exit
 		if (event.type == sf::Event::Closed) {
-			// window->close();
 			window.close(); 
-			// delete window;
 			return 1;
 		}
 	}
