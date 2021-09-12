ea843c84f6e50e741c52755b021698395e91ee86
kevin16777216 <k1029384756c@gmail.com>
2021-09-12 11:31:39 -0400

    last for the day

--- GameLib/Button.cpp
+++ GameLib/Button.cpp
@@ -85,8 +85,8 @@ void Button::notify(Layer& layer, int status)
 bool Button::checkHover()
 {
 	sf::Vector2i pos = sf::Mouse::getPosition(*screen);
-	float px = 0.0f + sf::Mouse::getPosition(*screen).x;
-	float py = 0.0f + sf::Mouse::getPosition(*screen).y;
+	float px = 0.0f + pos.x;
+	float py = 0.0f + pos.y;
 	return isHovering = hitbox.getGlobalBounds().contains(sf::Vector2f(px,py));
 }
 bool Button::checkClick()
