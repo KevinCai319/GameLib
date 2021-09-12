397537ffd3aa4e0843833faa1b0fcd5fc2193906
kevin16777216 <k1029384756c@gmail.com>
2021-09-12 17:10:23 -0400

    save

--- GameLib/Physical.cpp
+++ GameLib/Physical.cpp
@@ -1,12 +1,33 @@
 #include "Physical.hpp"
 
+
+float Physical::dotProduct(sf::Vector2f p0, sf::Vector2f p1)
+{
+    return (p0.x * p1.x) + (p0.y * p1.y);
+}
+std::vector<sf::Vector2f> Physical::getAxis(const sf::Shape& shape) {
+    const int length = shape.getPointCount();
+    std::vector <sf::Vector2f> axes;
+    for (int i = 0; i < length;i++) {
+        sf::Vector2f p0 = shape.getPoint(i);
+        sf::Vector2f p1 = shape.getPoint(i == length-1?0:i);
+        sf::Vector2f axis = sf::Vector2f(p1.y - p0.y, p0.x - p1.x);
+        axes.push_back(axis / sqrt(powf(axis.x,2)+powf(axis.y,2)));
+    }
+    return axes;
+}
 const bool Physical::isColliding(Physical& other)
 {
     //estimate collision
     bool est = other.getShape().getGlobalBounds().intersects(getShape().getGlobalBounds());
     if (est) {
-        //if close, do GJK or SAT.
-        //FIX: GJK or SAT
+        return true; //TODO:remove this
+        const sf::Shape& otherBound = other.getShape();
+        const sf::Shape& myBound = getShape();
+        int otherSize = otherBound.getPointCount();
+        std::vector<sf::Vector2f> p0a = getAxis(myBound);
+        std::vector<sf::Vector2f> p1a = getAxis(otherBound);
+        //TODO: get this working
         return true;
     }
     return false;
--- GameLib/Physical.hpp
+++ GameLib/Physical.hpp
@@ -4,6 +4,8 @@ class Physical
 {
 	public:
 		virtual const sf::Shape& getShape() = 0;
+		float dotProduct(sf::Vector2f p0, sf::Vector2f p1);
+		std::vector<sf::Vector2f> getAxis(const sf::Shape& shape);
 		virtual const bool isColliding(Physical& other);
 	protected:
 
