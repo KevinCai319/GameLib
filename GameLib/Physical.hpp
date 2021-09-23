825d3ae9adfd2343f22f9a8803ab13e0a3351873
kevin16777216 <k1029384756c@gmail.com>
2021-09-23 15:29:15 -0400

    Revert "merge conflict yay"
    This reverts commit acaba350a2a8349547e5cad37cc2800d37579aa4.

--- GameLib/Physical.hpp
+++ GameLib/Physical.hpp
@@ -9,7 +9,7 @@ class Physical:public Layer
 		virtual const sf::FloatRect& getAABB();
 		float dotProduct(sf::Vector2f p0, sf::Vector2f p1);
 		std::vector<sf::Vector2f> getAxis(const sf::Shape& shape);
-		virtual const bool isColliding(Physical& other) const = 0;
+		virtual const bool isColliding(Physical& other);
 		static bool isABBBColling(sf::FloatRect a, sf::FloatRect b);
 	protected:
 
